e4e55213f388b0aba7d740e7b5b4f405e705f9ce
Micavanco <micavanco@gmail.com>
2019-09-11 16:35:36 +0200

    Fixed application structure and combine with MongoDB

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const Joi = require('@hapi/joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        min: 3
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 5
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String,
+        required: true
+    },
+    createdAt: { type: Date, default: Date.now}
+});
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(body) {
+    const schema = {
+        username: Joi.string().min(3).required(),
+        password: Joi.string().min(5).required()
+    };
+
+    return Joi.validate(body, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
--- routes/users.js
+++ routes/users.js
@@ -1,77 +1,127 @@
+const {User, validate} = require('../models/user');
 const express = require('express');
 const router = express.Router();
-const Joi = require('@hapi/joi');
 
-const users = [
-    {id: 1, username: 'John', password: 'Walker'},
-    {id: 2, username: 'Bob', password: 'admin2'}
-];
+// const users = [
+//     {id: 1, username: 'John', password: 'Walker'},
+//     {id: 2, username: 'Bob', password: 'admin2'}
+// ];
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const users = await User.find().sort('username');
     res.send(users);
 });
 
-router.post('/', (req, res) => {
-    const { error } = validateUser(req.body);
-
-    if(error)
-        res.status(400).send(error.details[0].message);
-    else
-    {
-        const user = {id: users.length+1, username: req.body.username, password: req.body.password};
-        users.push(user);
+router.post('/', async (req, res) => {
+    try{
+        let user = await new User({
+            username: req.body.username,
+            password: req.body.password,
+            name: req.body.name,
+            surname: req.body.surname
+        });
+        user = await user.save();
         res.send(user);
+    }catch (e) {
+        res.status(400).send(e.message);
     }
 });
 
-router.put('/:id', (req, res) => {
-    const user = users.find(u => u.id === parseInt(req.params.id));
-
-    if(!user)
-        return res.status(404).send(`User of given id ${req.params.id} does not exists`);
+router.put('/', async (req, res) => {
+    let user = await User.findOne({username: req.body.username});
+    if(!user) return res.status(404).send(`User of given username ${req.body.username} does not exists`);
 
 
-    const { error } = validateUser(req.body);
-
-    if(error)
-        res.status(400).send(error.details[0].message);
-    else
-    {
-        user.username = req.body.username;
+    try {
         user.password = req.body.password;
-        res.send(user);
+        user.name = req.body.name;
+        user.surname = req.body.surname;
+
+        const result = await user.save();
+        res.send(result);
+    }catch (e) {
+        res.status(400).send(e.message);
     }
 });
 
-router.get('/:id', (req, res) => {
-    //res.send(`Id: ${req.params.id}, Username: ${req.params.username}, Password: ${req.params.password}`);
-    const user = users.find(u => u.id === parseInt(req.params.id));
+router.get('/:username', async (req, res) => {
+    const user = await User.find({username: req.params.username});
 
-    if(user)
+    if(user.length !== 0)
         res.send(user);
     else
-        res.status(404).send(`User of given id ${req.params.id} does not exists`);
+        res.status(404).send(`User of given username ${req.params.username} does not exists`);
 });
 
-router.delete('/:id', (req, res) => {
-    const user = users.find(u => u.id === parseInt(req.params.id));
+router.delete('/:username', async (req, res) => {
+    let user = await User.deleteOne({username: req.params.username});
+    if(!user) return res.status(404).send(`User of given username ${req.params.username} does not exists`);
 
-    if(user){
-        const index = users.indexOf(user);
-        users.splice(index, 1);
-        res.send(user);
-    }
-    else
-        res.status(404).send(`User of given id ${req.params.id} does not exists`);
+    res.send(user);
 });
-
-function validateUser(body) {
-    const schema = {
-        username: Joi.string().min(3).required(),
-        password: Joi.string().min(5).required()
-    };
-
-    return Joi.validate(body, schema);
-}
 
 module.exports = router;
+
+//
+// async function createUser(username, password) {
+//     const user = new User({
+//         username: username,
+//         password: password,
+//         name: 'Matt',
+//         surname: 'Popcorn'
+//     });
+//
+//     const result = await user.save();
+//     console.log(result);
+// }
+//
+// async function getUsers() {
+//     // eq  (equal)
+//     // ne  (not equal)
+//     // gt  (greater than)
+//     // gte (greater than or equal to)
+//     // lt  (less than)
+//     // lte (less than or equal to)
+//     // in
+//     // nin (not in)
+//
+//     const pageNumber = 2;
+//     const pageSize = 10;
+//
+//     const users = await User
+//         .find( {createdAt: {$lt: Date.now()}})
+//         //.or([{username: /o$/i}, {password: '1234'}]) // case insensitive
+//         .or([{username: /.*ca.*/i}, {password: '1234'}]) // contains 'ca' case insensitive
+//         //.or([{username: /^m/}, {password: '1234'}])
+//         //.sort( {username: 1})
+//         //.skip((pageNumber - 1) * pageNumber)
+//         .select({username: 1,password:1});
+//     //.count();
+//     console.log(users);
+// }
+//
+// async function updateUsers(id) {
+//     // const user = await User.findById(id);
+//     const user = await User.update({_id: id}, {
+//         $set: {
+//             name : "Tony",
+//             surname : "Hawwk"
+//         }
+//     });
+//     // if(!user) return;
+//     // user.name = "Tony";
+//     // user.surname = "Hawwk";
+//     // user.set({
+//     //     name : "Tony",
+//     //     surname : "Hawwk"
+//     // });
+//     console.log(user);
+// }
+// // updateUsers('5d6f940299cfe20c54b98213');
+//
+// async function removeUser(id) {
+//     const user = await User.deleteOne({_id: id});
+//     console.log(user);
+// }
+
+
